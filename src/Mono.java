a4bde6d8252248c5a9f7d2c9675e5966d8eec7cb
arathnim <arathnim@gmail.com>
2014-12-20 14:18:12 -0600

    added a cat ^.^

--- src/Mono.java
+++ src/Mono.java
@@ -44,7 +44,11 @@ public class Mono extends JFrame {
         m.setFocusable(true);
         m.addKeyListener(new listner());
         
+        
+        //displayCat();
+        displaySmallerCat();
         writePrompt();
+        
         
     }
     
@@ -52,6 +56,63 @@ public class Mono extends JFrame {
     {
     	panel.writeString(" Arathnim ",255,255,255,25,100,100);
         panel.writeString(""+ (char) 18 + (char) 17+" ",25,100,100);
+    }
+    
+    public static void displayCat()
+    {
+    	BufferedImage cat = null;
+    	try {
+			cat = ImageIO.read(new File("res/cat.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	int image[] = new int[600];
+    	cat.getRGB(0, 0, 30, 20, image, 0, 30);
+    	
+    	for(int k=0;k<600;k++)
+    	{
+    			Color c = new Color(image[k]);
+    			panel.writeString(""+(char) 15+(char) 15,c.getRed(),c.getGreen(),c.getBlue());
+    		
+    		if(k%30 == 29)
+    			panel.writeString("\n");
+    	}
+    	
+    	panel.writeString("\n");
+    }
+    
+    public static void displaySmallerCat()
+    {
+    	BufferedImage cat = null;
+    	try {
+			cat = ImageIO.read(new File("res/cat.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	int image[] = new int[600];
+    	cat.getRGB(0, 0, 30, 20, image, 0, 30);
+    	
+    	for(int k=0;k<600;k++)
+    	{
+    		if((k/30) %2 == 0)
+    		{
+    			Color c = new Color(image[k]);
+    			Color c2 = new Color(image[k+30]);
+    			panel.writeString(""+(char) 16,c2.getRed(),c2.getGreen(),c2.getBlue(),c.getRed(),c.getGreen(),c.getBlue());
+    			if(k%30 == 29)
+        			panel.writeString("\n");
+    		}
+    		
+    	}
+    	
+    	panel.writeString("\n");
+    }
+    
+    public static void writeDoubleRow(int r, int g, int b,int r2, int g2, int b2)
+    {
+    	panel.writeString(""+(char) 16, r, g, b, r2, g2, b2);
     }
 }
 
